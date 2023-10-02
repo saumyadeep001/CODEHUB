15d5de804538bbb75f5c72a672e031525e349d05
Saumyadeep Ghosh <121515207+deep230@users.noreply.github.com>
2023-10-02 17:47:18 +0530

    Time: 7 ms (50.36%), Space: 7.4 MB (23.24%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -14,3 +14,22 @@ public:
     }
 };
 
+
+// class Solution {
+// public:
+//     bool isIsomorphic(string s, string t) {
+//         vector<int>a(128,-1);
+//         vector<int>b(128,-1);
+//         for(int i=0;i<s.size();i++)
+//         {
+            
+//             if(a[s[i]]!=b[t[i]])
+//             {
+//                 return false;
+//             }
+//             a[s[i]]=i;
+//             b[t[i]]=i;
+//         }
+//         return true;
+//     }
+// };
