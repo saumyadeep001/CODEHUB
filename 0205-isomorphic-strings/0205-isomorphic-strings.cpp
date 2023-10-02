9740f573e5c2dbea2550bd56734e023dc28ae7a7
Saumyadeep Ghosh <121515207+deep230@users.noreply.github.com>
2023-10-02 17:45:50 +0530

    Time: 5 ms (68.43%), Space: 7.4 MB (15.54%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        vector<int>a(128,-1);
-        vector<int>b(128,-1);
-        for(int i=0;i<s.size();i++)
-        {
-            
-            if(a[s[i]]!=b[t[i]])
-            {
-                return false;
-            }
-            a[s[i]]=i;
-            b[t[i]]=i;
+        unordered_map<char , char> m1,m2;
+        for(int i =0;i<s.length();i++){
+           if(m1[s[i]] && m1[s[i]] !=t[i]) 
+               return 0;
+            if (m2[t[i]] && m2[t[i]]!=s[i])
+                return 0;
+              m1[s[i]]=t[i];
+            m2[t[i]]=s[i];
         }
-        return true;
+        return 1;
     }
 };
+
