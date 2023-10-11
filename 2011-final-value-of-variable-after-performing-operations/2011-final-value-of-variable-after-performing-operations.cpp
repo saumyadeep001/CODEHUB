543e42ce684434f8d4c3cd7e16d565efbe7f80cc
Saumyadeep Ghosh <121515207+deep230@users.noreply.github.com>
2023-10-11 18:31:31 +0530

    Time: 5 ms (69.52%), Space: 14.4 MB (22.84%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -4,18 +4,9 @@ public:
     {
         int X = 0;
         for(int i=0;i<operations.size();i++)
-        {
-          
-             if(operations[i] == "++X" || operations[i]=="X++")
-            {
-                ++X;
-            }
-          
-             else if(operations[i] == "--X" || operations[i] == "X--")
-            {
-                --X;
-            }
-        }
+        { if(operations[i] == "++X" || operations[i]=="X++")  ++X;
+           else  --X;
+      }
         return X;
     }
 };
