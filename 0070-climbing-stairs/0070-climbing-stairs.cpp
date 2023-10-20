5e96767a6df125d168711e1e22d5203e9099d48f
Saumyadeep Ghosh <121515207+deep230@users.noreply.github.com>
2023-10-20 15:49:41 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (58.49%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,34 @@
 class Solution {
+// public:
+//     int climbStairs(int n) {
+//         if(n==0) return 1;
+//         if(n==1) return 1;
+//         if(n==2) return 2;
+//         if(n==3) return 3;
+//         if(n==4) return 5;
+//         if(n==5)  return 8;
+        
+        
+//         long long res = climbStairs(n-1) + climbStairs(n-2);
+//         return res;
+//     }
+// };
+
+
 public:
     int climbStairs(int n) {
-        if(n==0) return 1;
-        if(n==1) return 1;
-        if(n==2) return 2;
-        if(n==3) return 3;
-        if(n==4) return 5;
-        if(n==5)  return 8;
-        
-        
-        long long res = climbStairs(n-1) + climbStairs(n-2);
-        return res;
+    // base cases
+    if(n <= 0) return 0;
+    if(n == 1) return 1;
+    if(n == 2) return 2;
+    
+    int a = 2,b=1,c=0;
+   
+  for(int i=2; i<n; i++){
+    	c = a + b;
+    	b = a;
+        a = c;
     }
-};
+    return c;
+}
+};    
