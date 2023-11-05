9fa8fffe08e3c93fecdaa078e023b8bf3d0fd8ec
Saumyadeep Ghosh <121515207+deep230@users.noreply.github.com>
2023-11-05 16:51:39 +0530

    Time: 2 ms (77.61%), Space: 6.5 MB (19.90%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -4,18 +4,14 @@ public:
         if (n <= 1)
             return n;
 
-        vector<int> dp(n + 1, -1); 
+        vector<int> dp(n + 1);
         dp[0] = 0;
         dp[1] = 1;
 
-        return helper(n, dp);
-    }
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
 
-    int helper(int n, vector<int>& dp) {
-        if (dp[n] != -1)
-            return dp[n];
-
-        dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
         return dp[n];
     }
 };
