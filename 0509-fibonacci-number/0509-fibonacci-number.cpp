b6a6856b4181128b4a3d6bbdac0eefee9ec88847
Saumyadeep Ghosh <121515207+deep230@users.noreply.github.com>
2023-11-06 19:55:08 +0530

    Time: 2 ms (77.51%), Space: 6.4 MB (39.25%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -14,6 +14,21 @@ public:
         
         dp[n] =topDownSol(n-1,dp) + topDownSol(n-2,dp);
          return dp[n];
+    } 
+    
+    
+    int bottomUpSol(int n){
+      vector<int> dp(n+1,-1);
+        dp[0] =0;
+        if(n==0) return dp[0];
+        dp[1] =1;
+        if(n==1) return dp[1];
+        
+        for(int i =2; i<=n ; i++){
+        dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        return dp[n];
     }
     
     
@@ -21,7 +36,9 @@ public:
         // int ans = recSol(n);
         // return ans;
         
-        vector<int> dp(n+1, -1);
-        return topDownSol(n, dp);
+        // vector<int> dp(n+1, -1);
+        // return topDownSol(n, dp);
+        
+        return bottomUpSol(n);
     }
 };
