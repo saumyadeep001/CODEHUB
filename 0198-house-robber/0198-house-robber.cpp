e84134291f55dcb8922357cb60f7f3525005bb05
Saumyadeep Ghosh <121515207+deep230@users.noreply.github.com>
2023-11-13 10:15:52 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (67.37%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -25,13 +25,34 @@ public:
          dp[n] = max(include, exclude);
         return dp[n];
     }
+    
+    
+    
+    int solveUsingTab(vector<int>& nums,int n){
+        vector<int> dp(n+1,0);
+        dp[0]=nums[0];
+        
+    for(int i =1 ; i<=n ; i++){
+       int temp =0;
+        if(i-2 >= 0) temp = dp[i-2];
+        
+        int include = temp + nums[i];
+        int exclude = dp[i-1] + 0;
+        
+        dp[i] = max(include, exclude);
+            
+    }
+        return dp[n];  
+    }
 
     int rob(vector<int>& nums) {
         int n = nums.size()-1;
         // return solveUsingRecursion(nums, n - 1); 
         
-        vector<int> dp(n+1 ,-1);
-        return topDown(nums,n,dp);
+//         vector<int> dp(n+1 ,-1);
+//         return topDown(nums,n,dp);
+        
+        return solveUsingTab(nums,n);
         
         
     }
