e254dd680e340fc418a9e629a73244027aecff4a
Saumyadeep Ghosh <121515207+saumyadeep001@users.noreply.github.com>
2023-12-04 20:19:37 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (98.61%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -1,19 +1,21 @@
-class Solution {
-public:
-    bool isPerfectSquare(int num) {
-      int i = 1;
-      while(num > 0){
-          num -= i;
-          i += 2;
-          if(!num) return true;     
-      }  
-        return false;
-    }
-};
-
 // class Solution {
 // public:
 //     bool isPerfectSquare(int num) {
-        
+//       int i = 1;
+//       while(num > 0){
+//           num -= i;
+//           i += 2;
+//           if(!num) return true;     
+//       }  
+//         return false;
 //     }
 // };
+
+class Solution {
+public:
+      bool isPerfectSquare(int num) {
+        long int i = 1;
+        for(;i*i<num;i++);
+        return i*i==num;
+    }
+};
