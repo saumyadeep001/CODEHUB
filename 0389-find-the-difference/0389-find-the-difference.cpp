42ab7fa4ae6bfb640afd676c5216193c5385fb00
Saumyadeep Ghosh <121515207+saumyadeep001@users.noreply.github.com>
2024-01-07 18:43:07 +0530

    Time: 4 ms (47.57%), Space: 7 MB (52.27%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,12 +1,34 @@
+// class Solution {
+// public:
+//     char findTheDifference(string s, string t) {
+//         sort(s.begin(),s.end());
+//         sort(t.begin(),t.end());
+//         for(int i =0;i<s.size();i++){
+//             if(s[i]!=t[i])
+//                 return t[i];
+//         }
+//         return t[s.size()];
+//     }
+// };
+
+// class Solution{
+// public:
+//     char findTheDifference(string s, string t){
+//      int sum1=0,sum2=t.length();
+//         for(int i =0;i<s.length();i++){
+//           sum1 += s[i];
+//           sum2 += t[i];
+//         }
+//         return char(abs(sum2-sum1));
+//     }
+// };
 class Solution {
 public:
-    char findTheDifference(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        for(int i =0;i<s.size();i++){
-            if(s[i]!=t[i])
-                return t[i];
-        }
-        return t[s.size()];
-    }
+	char findTheDifference(string s, string t) {
+		char ans = t[s.size()];
+		for(int i = 0; i < s.size(); i++){
+			ans ^= s[i] ^ t[i];
+		}
+		return ans;
+	}
 };
