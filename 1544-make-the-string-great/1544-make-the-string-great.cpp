33b917919755876031128f1022c25f44413fbdc8
Saumyadeep Ghosh <121515207+saumyadeep001@users.noreply.github.com>
2024-04-05 09:57:54 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (98.61%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        int n = s.size();
+        for (int i = 0; i < n - 1;) {
+            if (abs(s[i] - s[i + 1]) == 32) {
+                s.erase(i, 2); // Erase the current character and its adjacent one
+                n -= 2; // Update the size of the string after erasing
+                i = max(i - 1, 0); // Update the loop index to check the characters before the erased ones
+            } else {
+                i++; // Move to the next character
+            }
+        }
+        return s;
+    }
+};
+
+
