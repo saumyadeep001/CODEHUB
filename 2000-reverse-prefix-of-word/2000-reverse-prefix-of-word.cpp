0c963a339eaa2f9dd870b58c8d8a6808171faf67
Saumyadeep Ghosh <121515207+saumyadeep001@users.noreply.github.com>
2024-05-01 10:31:26 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (65.62%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -1,12 +1,23 @@
+// class Solution {
+// public:
+//     string reversePrefix(string word, char ch) {
+//         for(int i =0;i<word.size();i++){
+//           if(word[i]==ch){
+//            reverse(word.begin(),word.begin()+i+1);
+//               break;    
+//           }
+//         }
+//         return word;
+//     }
+// };
+
+
+// using purily stl function and it means use of reverse and to find
+
 class Solution {
 public:
     string reversePrefix(string word, char ch) {
-        for(int i =0;i<word.size();i++){
-          if(word[i]==ch){
-           reverse(word.begin(),word.begin()+i+1);
-              break;    
-          }
-        }
+        reverse(word.begin(),word.begin()+word.find(ch)+1);
         return word;
     }
 };
