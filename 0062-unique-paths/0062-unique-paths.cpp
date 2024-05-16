43e2d6919e11897f30bc0dc75a402f615fb94de2
Saumyadeep Ghosh <121515207+saumyadeep001@users.noreply.github.com>
2024-05-16 19:58:25 +0530

    Time: 4 ms (12.99%), Space: 7.7 MB (44.67%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,22 +1,44 @@
+// class Solution {
+// public:
+//     int uniquePaths1(int m , int n,vector<vector<int>>&dp){
+//          if(m==0 and n ==0)  return 1;
+//         if(m<0 or n<0) return 0;
+        
+//         if(dp[m][n]!=-1) return dp[m][n];
+        
+//         int up = uniquePaths1(m-1,n ,dp);
+//         int down = uniquePaths1(m,n-1,dp);
+        
+//         return dp[m][n]=up+down;
+        
+//     }
+//     int uniquePaths(int m, int n) {
+//         vector<vector<int>> dp(m, vector<int>(n,-1));
+//        return uniquePaths1(m-1,n-1,dp);
+//     }
+// };
+
+
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
-    int uniquePaths1(int m , int n,vector<vector<int>>&dp){
-         if(m==0 and n ==0)  return 1;
-        if(m<0 or n<0) return 0;
+    int uniquePaths1(int m, int n, vector<vector<int>>& dp) {
+        if (m == 0 && n == 0) return 1;
+        if (m < 0 || n < 0) return 0;
         
-        if(dp[m][n]!=-1) return dp[m][n];
+        if (dp[m][n] != -1) return dp[m][n];
         
-        int up = uniquePaths1(m-1,n ,dp);
-        int down = uniquePaths1(m,n-1,dp);
+        int up = uniquePaths1(m - 1, n, dp);
+        int down = uniquePaths1(m, n - 1, dp);
         
-        return dp[m][n]=up+down;
-        
+        return dp[m][n] = up + down;
     }
+    
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n,-1));
-       return uniquePaths1(m-1,n-1,dp);
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return uniquePaths1(m - 1, n - 1, dp);
     }
 };
-
-
 
