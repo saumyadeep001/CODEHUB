3a82a5a95da6ed990e54a765560c75f22763fab5
Saumyadeep Ghosh <121515207+saumyadeep001@users.noreply.github.com>
2024-05-28 17:46:48 +0530

    Time: 164 ms (48.94%), Space: 98.7 MB (35.73%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     
-    bool solveRec(int i , vector<int>& nums,int target,vector<vector<int>>& dp){
+    bool solveRec(int i , vector<int>& nums,int target){
         int n = nums.size();
         if(i>=n) return 0;
         if(target<0) return 0;
         if(target==0) return 1;
         
-        int include = solveRec(i+1,nums,target-nums[i],dp);
-        int exclude = solveRec(i+1,nums,target,dp);
+        int include = solveRec(i+1,nums,target-nums[i]);
+        int exclude = solveRec(i+1,nums,target );
         
         return (include or exclude);
         
@@ -26,10 +26,12 @@ public:
         int exclude = solveMem(i+1,nums,target,dp);
         
         dp[i][target]= (include or exclude);
-        
+         
         return dp[i][target];
         
     }
+    
+  
     
     bool canPartition(vector<int>& nums) {
         int sum =0;
@@ -41,15 +43,9 @@ public:
         int target = sum/2;
         int index =0;
         // int ans = solveRec(index,nums,target);
-        // it will show TLE
-        
-        
+        // it will show TLE    
         vector<vector<int>> dp(nums.size(),vector<int>(target+1,-1));
-        int ans = solveMem(index,nums,target,dp);
-
-        
-        return ans ;
-        
-        
+        int ans = solveMem(index,nums,target,dp);      
+        return ans ;  
     }
 };
