a856bf866ac7dbb314ef61edb7be9bdd2c936ca8
Saumyadeep Ghosh <121515207+saumyadeep001@users.noreply.github.com>
2024-06-03 14:52:59 +0530

    Time: 11 ms (51.74%), Space: 19.9 MB (31.61%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,22 +1,58 @@
+// class Solution {
+// public:
+//     int sol(int n, int target, vector<int> &coins, vector<vector<int>>& dp) {
+//         if (n == 0) {
+          
+//             return (target % coins[0] == 0);
+//         }
+//         if (dp[n][target] != -1) return dp[n][target];
+
+//         int notTake = sol(n-1, target, coins, dp);
+//         int take = 0;
+//         if (coins[n] <= target) take = sol(n, target - coins[n], coins, dp);
+        
+//         dp[n][target] = take + notTake;
+//         return dp[n][target];
+//     }
+
+//     int change(int amount, vector<int>& coins) {
+//         int n = coins.size();
+//         vector<vector<int>> dp(n, vector<int>(amount+1, 0));
+//         //  return sol(n-1, amount, coins, dp);
+//         for(int i =1;i<n;i++){
+//             for(int j = 0;j<=amount;j++){
+//             int nottake = dp[i-1][j];;
+//             int take = 0;
+//             take = dp[i][j-coins[n]];
+//             dp[i][j]=take +nottake;
+            
+//         }
+//     }
+//     return dp[n-1][amount];
+//     }
+// };
+
 class Solution {
 public:
-    int sol(int n, int target, vector<int> &coins, vector<vector<int>>& dp) {
-        if (n == 0) {
-            return (target % coins[0] == 0);
-        }
-        if (dp[n][target] != -1) return dp[n][target];
-
-        int notTake = sol(n-1, target, coins, dp);
-        int take = 0;
-        if (coins[n] <= target) take = sol(n, target - coins[n], coins, dp);
-        
-        dp[n][target] = take + notTake;
-        return dp[n][target];
-    }
-
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        return sol(n-1, amount, coins, dp);
+        vector<vector<int>> dp(n+1, vector<int>(amount+1, 0));
+
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= amount; j++) {
+                int nottake = dp[i-1][j];
+                int take = 0;
+                if (j >= coins[i-1]) {
+                    take = dp[i][j-coins[i-1]];
+                }
+                dp[i][j] = take + nottake;
+            }
+        }
+
+        return dp[n][amount];
     }
 };
