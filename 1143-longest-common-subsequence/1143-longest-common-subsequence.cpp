1f74a91c2682de0d65d2fedf789e7b218007cfa5
Saumyadeep Ghosh <121515207+saumyadeep001@users.noreply.github.com>
2024-06-03 18:10:05 +0530

    Time: 18 ms (82.25%), Space: 25.5 MB (73.17%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -17,8 +17,23 @@ public:
         int n = text1.length();
         int m = text2.length();
 
-        vector<vector<int>> dp(n, vector<int>(m, -1));
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
 
-        return sol(n - 1, m - 1, text1, text2, dp);
+        // return sol(n - 1, m - 1, text1, text2, dp);
+
+        for(int i = 0; i <= n; i++) dp[i][0] = 0;
+        for(int j = 0; j <= m; j++) dp[0][j] = 0;
+  
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (text1[i - 1] == text2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        
+        return dp[n][m];
     }
 };
