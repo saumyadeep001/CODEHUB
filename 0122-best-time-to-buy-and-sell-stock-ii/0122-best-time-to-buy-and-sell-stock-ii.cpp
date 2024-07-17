a0949ebf98252f4b4293c41a7736b01a539b1fe8
Saumyadeep Ghosh <121515207+saumyadeep001@users.noreply.github.com>
2024-07-17 15:40:00 +0530

    Time: 8 ms (36.96%), Space: 18.2 MB (20.31%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int sol(int i, int buy, int n, vector<int> &prices, vector<vector<int>> &dp) {
+    int solMem(int i, int buy, int n, vector<int> &prices, vector<vector<int>> &dp) {
         if (i == n) return 0;
         if (dp[i][buy] != -1) return dp[i][buy];
 
         int profit = 0;
         if (buy) {
-            profit = max(-prices[i] + sol(i+1, 0, n, prices, dp), sol(i+1, 1, n, prices, dp));
+            profit = max(-prices[i] + solMem(i+1, 0, n, prices, dp), solMem(i+1, 1, n, prices, dp));
         } else {
-            profit = max(prices[i] + sol(i+1, 1, n, prices, dp), sol(i+1, 0, n, prices, dp));
+            profit = max(prices[i] + solMem(i+1, 1, n, prices, dp), solMem(i+1, 0, n, prices, dp));
         }
 
         return dp[i][buy] = profit;
@@ -16,7 +16,20 @@ public:
 
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));
-        return sol(0, 1, n, prices, dp);
+        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int buy = 0; buy < 2; buy++) {
+                int profit = 0;
+                if (buy) {
+                    profit = max(-prices[i] + dp[i + 1][0], dp[i + 1][1]);
+                } else {
+                    profit = max(prices[i] + dp[i + 1][1], dp[i + 1][0]);
+                }
+                dp[i][buy] = profit;
+            }
+        }
+
+        return dp[0][1];
     }
 };
